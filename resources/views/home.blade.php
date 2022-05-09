9cf71fd667b7291aaed4ec419e9e4b2461c14e1a
josh <hannsflip@gmail.com>
2022-05-09 23:25:03 +0200

    add 1st part of moneyfan

---
+++ resources/views/home.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="/img/profile-pic.jpg" alt="" class="rounded-circle" style= "max-height: 200px;">
+        </div>
+        <div class="col-9 pt-5">
+            <div>
+                <h1>username</h1>
+            </div>
+            <div class="d-flex">
+                <div style= "padding-right: 1rem;">
+                    <strong>153</strong> post
+                </div>
+                <div style= "padding-right: 1rem;">
+                    <strong>20k</strong> followers
+                </div>
+                <div style= "padding-right: 1rem;">
+                    <strong>523</strong> following
+                </div>
+            </div>
+            <div class="pt-4 font-weight-bold">
+                <strong>name</strong>
+            </div>
+            <div class="">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pulvinar mattis nunc sed blandit. Nascetur ridiculus mus mauris vitae ultricies leo. Massa tempor nec feugiat nisl pretium. Sed nisi lacus sed viverra tellus in hac habitasse. In aliquam sem fringilla ut morbi. Arcu dictum varius duis at consectetur lorem donec massa. A arcu cursus vitae congue. Tellus id interdum velit laoreet. Pulvinar mattis nunc sed blandit libero. Sit amet nisl purus in mollis nunc. At lectus urna duis convallis convallis tellus. Mauris sit amet massa vitae tortor. Tellus integer feugiat scelerisque varius morbi enim nunc.
+            </div>
+            <div><a href="">www.moneyfan.com</a></div>
+        </div>
+    </div>
+    <div class="row pt-5">
+        <div class="col-4">  <img src="/img/post.png" class="w-100">  </div>
+        <div class="col-4">  <img src="/img/post.png" class="w-100">  </div>
+        <div class="col-4">  <img src="/img/post.png" class="w-100">  </div>
+    </div>
+</div>
+@endsection
